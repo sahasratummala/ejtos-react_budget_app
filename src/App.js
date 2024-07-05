c6141fcfff00eff125f5d2ccc94bfe6f2f42006a
Sahasra Tummala <sahasratummala73@gmail.com>
2024-07-05 09:30:42 -0400

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { AppProvider } from './context/AppContext';
@@ -7,33 +7,41 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import CurrencyDropdown from './components/CurrencyDropdown';
 
 const App = () => {
+    const [currency, setCurrency] = useState('£');
+
+    const handleCurrencyChange = (event) => {
+        setCurrency(event.target.value);
+    };
+
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
                     <div className='col-sm'>
-                        <Budget />
+                        <Budget currency={currency} />
                     </div>
                     <div className='col-sm'>
-                        <RemainingBudget />
+                        <RemainingBudget currency={currency} />
                     </div>
                     <div className='col-sm'>
-                        <ExpenseTotal />
+                        <ExpenseTotal currency={currency} />
+                    </div>
+                    <div className='col-sm'>
+                        <CurrencyDropdown currency={currency} onCurrencyChange={handleCurrencyChange} />
                     </div>
                 </div>
-                <h3 className='mt-3'>Allocation</h3>
-                <div className='row '>
-                    <div className='col-sm'>
-                        <ExpenseList />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Change allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
-                        <AllocationForm/>
+                        <ExpenseList currency={currency} />
+                    </div>
+                </div>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm currency={currency} />
                     </div>
                 </div>
             </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,38 +1,43 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { TiDelete, TiMinus, TiPlus } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
-
+    const { dispatch, currency } = useContext(AppContext);
     const handleDeleteExpense = () => {
         dispatch({
             type: 'DELETE_EXPENSE',
             payload: props.id,
         });
     };
-
     const increaseAllocation = (name) => {
         const expense = {
             name: name,
             cost: 10,
         };
-
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
         });
-
     }
-
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>{currency} {props.cost}</td>
+            <td><TiPlus size='1.5em' onClick={event => increaseAllocation(props.name)}></TiPlus></td>
+            <td><TiMinus size='1.5em' onClick={event => decreaseAllocation(props.name)}></TiMinus></td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
-
-export default ExpenseItem;
+export default ExpenseItem
